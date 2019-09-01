7034c9f48770a8d5297d56155a0161feb0a5dddd
yejx <yejinxiong01@126.com>
2019-09-02 00:40:33 +0800

    jpa操作CRUD

---
+++ src/main/java/com/hero/sell/business/productcategory/dao/ProductCategoryDao.java
@@ -0,0 +1,27 @@
+package com.hero.sell.business.productcategory.dao;
+
+import com.hero.sell.entities.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author yejx
+ * @Date 2019/8/25
+ */
+public interface ProductCategoryDao extends JpaRepository<ProductCategory, String> {
+    /**
+     * 根据商品名称查询
+     * @param categoryName
+     * @return
+     */
+    List<ProductCategory> findByCategoryName(String categoryName);
+
+    /**
+     * 根据商品类目编码查询
+     * @param categoryCodeList
+     * @return
+     */
+    List<ProductCategory> findByCategoryCodeIn(List<Integer> categoryCodeList);
+}
---
+++ src/main/java/com/hero/sell/business/productcategory/service/ProductCategoryService.java
@@ -0,0 +1,73 @@
+package com.hero.sell.business.productcategory.service;
+
+import com.hero.sell.business.productcategory.dao.ProductCategoryDao;
+import com.hero.sell.entities.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author yejx
+ * @Date 2019/8/26
+ */
+@Service("productCategoryService")
+public class ProductCategoryService {
+
+    @Autowired
+    private ProductCategoryDao productCategoryDao;
+
+    /**
+     * 查询所有
+     * @return
+     */
+    public List<ProductCategory> findAll() {
+        return productCategoryDao.findAll();
+    }
+
+    /**
+     * 根据ID查询(2.0后不能使用findOne()了)
+     * @param categoryId
+     * @return
+     */
+    public ProductCategory findById(String categoryId) {
+        ProductCategory productCategory = productCategoryDao.findById(categoryId).get();
+//        ProductCategory productCategory = productCategoryDao.getOne(categoryId);
+        return productCategory;
+    }
+
+    /**
+     * 根据商品名称查询
+     * @param categoryName
+     * @return
+     */
+    public List<ProductCategory> findByCategoryName(String categoryName) {
+        return productCategoryDao.findByCategoryName(categoryName);
+    }
+
+    /**
+     * 根据商品类目编码查询
+     * @return
+     */
+    public List<ProductCategory> findByCategoryCodeIn(List<Integer> categoryCodeList){
+        return productCategoryDao.findByCategoryCodeIn(categoryCodeList);
+    }
+
+    /**
+     * 新增/更新
+     * @param productCategory
+     */
+    public void saveOrUpdate(ProductCategory productCategory){
+        productCategoryDao.save(productCategory);
+    }
+
+    /**
+     * 删除
+     * @param categoryId
+     */
+    public void deleteById(String categoryId) {
+        productCategoryDao.deleteById(categoryId);
+    }
+
+}
