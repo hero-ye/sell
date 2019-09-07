feefab7705bf496ef80adcea641f6e30185a2374
yejx <yejinxiong01@126.com>
2019-09-07 17:50:11 +0800

    更新

--- src/main/java/com/hero/sell/SellApplication.java
+++ src/main/java/com/hero/sell/SellApplication.java
@@ -1,9 +1,7 @@
 package com.hero.sell;
 
-import com.hero.sell.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SellApplication {
@@ -11,11 +9,6 @@ public class SellApplication {
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
         System.out.println("已启动！");
-    }
-
-    @Bean
-    public IdWorker idWorker(){
-        return new IdWorker(1, 1);
     }
 
 }
