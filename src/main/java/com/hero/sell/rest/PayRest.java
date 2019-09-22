ddc9327334f4b46f95fa0c21c1824b00c7ae7ae4
yejx <yejinxiong01@126.com>
2019-09-22 18:29:42 +0800

    支付

--- src/main/java/com/hero/sell/rest/PayRest.java
+++ src/main/java/com/hero/sell/rest/PayRest.java
@@ -4,13 +4,14 @@ import com.hero.sell.dto.OrderDTO;
 import com.hero.sell.enums.ResultEnum;
 import com.hero.sell.exception.SellException;
 import com.hero.sell.service.OrderService;
+import com.hero.sell.service.PayService;
+import com.lly835.bestpay.model.PayResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * @Description 支付
@@ -25,9 +26,22 @@ public class PayRest {
     @Resource(name = "orderService")
     private OrderService orderService;
 
+    @Resource(name = "payService")
+    private PayService payService;
+
+
+
+    /**
+     * 发起支付
+     *
+     * @param orderId
+     * @param returnUrl
+     * @return
+     */
     @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public void creata(@RequestParam("orderId") String orderId,
-                       @RequestParam("returnUrl") String returnUrl){
+    public ModelAndView creata(@RequestParam("orderId") String orderId,
+                               @RequestParam("returnUrl") String returnUrl,
+                               Map<String, Object> param) {
         //1.查询订单
         OrderDTO orderDTO = orderService.findOrderDTOById(orderId);
         if (orderDTO == null) {
@@ -35,7 +49,35 @@ public class PayRest {
         }
 
         //2.发起支付
+        try {
+            PayResponse payResponse = payService.create(orderDTO);
+            param.put("payResponse", payResponse);
+        } catch (Exception e) {
+            PayResponse payResponse1 = new PayResponse();
+            payResponse1.setAppId("wxd898fcb01713c658");
+            payResponse1.setTimeStamp(System.currentTimeMillis() + "");
+            payResponse1.setNonceStr("bVsQpcfsKUAzO8r0");
+            payResponse1.setPackAge("prepay_id=wx2019091911112036b51eaddc0529394957");
+            payResponse1.setPaySign("78CA85306AB823156E1032EFB5BB1C76");
+            log.error(e.getMessage());
+            param.put("payResponse", payResponse1);
+        }
+        //支付成功后跳转的URL
+        param.put("returnUrl", returnUrl);
+        return new ModelAndView("pay/create", param);
+    }
 
+    /**
+     * 异步通知
+     * @param notifyData
+     */
+    @RequestMapping(value = "/notify", method = RequestMethod.POST)
+    public ModelAndView notify(@RequestBody String notifyData) {
+        //异步通知
+        payService.notify(notifyData);
+
+        //返回给微信处理结果
+        return new ModelAndView("pay/success");
     }
 
 }
--- src/main/java/com/hero/sell/service/PayService.java
+++ src/main/java/com/hero/sell/service/PayService.java
@@ -1,7 +1,10 @@
 package com.hero.sell.service;
 
 import com.hero.sell.dto.OrderDTO;
+import com.hero.sell.enums.ResultEnum;
+import com.hero.sell.exception.SellException;
 import com.hero.sell.utils.JsonUtil;
+import com.hero.sell.utils.MathUtil;
 import com.lly835.bestpay.enums.BestPayTypeEnum;
 import com.lly835.bestpay.model.PayRequest;
 import com.lly835.bestpay.model.PayResponse;
@@ -10,12 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * @Description
  * @Author yejx
  * @Date 2019/9/17
  */
-@Service
+@Service("payService")
 @Slf4j
 public class PayService {
 
@@ -24,17 +29,63 @@ public class PayService {
     @Autowired
     private BestPayServiceImpl bestPayService;
 
-    public void create(OrderDTO orderDTO) {
+    @Resource(name = "orderService")
+    private OrderService orderService;
+
+    /**
+     * 发起支付
+     * @param orderDTO
+     * @return
+     */
+    public PayResponse create(OrderDTO orderDTO) {
         PayRequest payRequest = new PayRequest();
         payRequest.setOpenid(orderDTO.getOrderId());
         payRequest.setOrderAmount(orderDTO.getOrderAmount().doubleValue());
         payRequest.setOrderId(orderDTO.getOrderId());
         payRequest.setOrderName(ORDER_NAME);
         payRequest.setPayTypeEnum(BestPayTypeEnum.WXPAY_H5);    //微信公众号支付
-        log.info("【微信支付】request={}", JsonUtil.toJson(payRequest));
+        log.info("【微信支付】发起支付，request={}", JsonUtil.toJson(payRequest));
         PayResponse payResponse = bestPayService.pay(payRequest);
-        log.info("【微信支付】response={}", JsonUtil.toJson(payResponse));
+        log.info("【微信支付】发起支付，response={}", JsonUtil.toJson(payResponse));
 
+        return payResponse;
+    }
+
+    /**
+     * 异步通知
+     * @param notifyData
+     * @return
+     */
+    public PayResponse notify(String notifyData) {
+        //1.验证签名
+        //2.支付的状态
+        //3.支付的金额
+        //4.支付人（下单人 == 支付人）
+        PayResponse payResponse = bestPayService.asyncNotify(notifyData);
+        log.info("【微信支付】异步通知，payResponse={}", JsonUtil.toJson(payResponse));
+
+        //查询订单
+        OrderDTO orderDTO = orderService.findOrderDTOById(payResponse.getOrderId());
+
+        //判断订单是否存在
+        if (orderDTO == null) {
+            log.error("【微信支付】异步通知，订单不存在，orderId={}", payResponse.getOrderId());
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+
+        //判断金额是否一致(判断0.10和0.1是否相等：相减后的值小于某个值即可)
+        if (!MathUtil.equals(payResponse.getOrderAmount(), orderDTO.getOrderAmount().doubleValue())) {
+            log.error("【微信支付】异步通知，订单金额不一致，orderId={},微信通知金额={},系统金额={}",
+                        payResponse.getOrderId(),
+                        payResponse.getOrderAmount(),
+                        orderDTO.getOrderAmount());
+            throw new SellException(ResultEnum.WXPAY_NOTIFY_MONEY_VERIFY_ERROR);
+        }
+
+        //修改订单支付状态
+        orderService.paid(orderDTO);
+
+        return payResponse;
     }
 
 }
